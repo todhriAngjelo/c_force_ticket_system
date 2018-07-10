26c4737a6523ff7339c112bb4a5724c47bc90844
RiCoGR <alkhs01@gmail.com>
2018-07-10 15:49:04 +0300

    fixed preview

--- testAngular/src/app/app.component.ts
+++ testAngular/src/app/app.component.ts
@@ -1,29 +1,18 @@
-import {Component, OnInit} from "@angular/core";
- import {AppService} from "./app.service";
- import {Tickets} from "./posts";
-import { MoviesService } from "./services/movies.service";
- 
- @Component({
-     selector: 'app-root',
-     templateUrl: './app.component.html',
-     styleUrls: ['./app.component.css'],
-     providers: [AppService]
- })
- export class AppComponent implements OnInit {
- 
-    genres: Array<Object>;
+import { Component } from '@angular/core';
+import {MoviesService} from '../app/services/movies.service';
 
-    constructor(private _moviesServices: MoviesService) {
-      this._moviesServices.getGenres().subscribe(res => {
-        this.genres = res.genres.slice(0, 20);
-      });
-    }
- 
-     
-     ngOnInit(): void {
-     }
-     
-    
-   
-   
- }
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  providers: [MoviesService]
+})
+export class AppComponent {
+  genres: Array<Object>;
+
+  constructor(private _moviesServices: MoviesService) {
+    this._moviesServices.getGenres().subscribe(res => {
+      this.genres = res.genres.slice(0, 20);
+    });
+  }
+}
