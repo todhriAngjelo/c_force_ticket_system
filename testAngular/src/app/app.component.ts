0ba1a5c9a896b14d5904874d332b0f07de88e06c
Rico <alkhs01@gmail.com>
2018-07-10 21:33:50 +0300

    Logout Button

--- testAngular/src/app/app.component.ts
+++ testAngular/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import {MoviesService} from '../app/services/movies.service';
+import { AuthService } from './services/auth/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -10,9 +12,16 @@ import {MoviesService} from '../app/services/movies.service';
 export class AppComponent {
   genres: Array<Object>;
 
-  constructor(private _moviesServices: MoviesService) {
+  constructor(private _moviesServices: MoviesService,
+              private router:Router,
+              public authService: AuthService) {
     this._moviesServices.getGenres().subscribe(res => {
       this.genres = res.genres.slice(0, 20);
     });
   }
+  logout() {
+    this.authService.logout();
+    this.router.navigate(['']);
+  }
+
 }
